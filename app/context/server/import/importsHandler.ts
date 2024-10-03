29e4d944551f2d08d00c58cad8b06bf6251ffc6f
abdulwahabtaqi <abdulwahabtaqihaider@gmail.com>
2024-10-03 21:41:36 +0500

    fixed minor bugs

--- app/context/server/import/importsHandler.ts
+++ app/context/server/import/importsHandler.ts
@@ -6,8 +6,8 @@ const importsHandler = async (data: ImportInput, csvFile: File) => {
 
     pageLoader?.setPageLoading(30);
     const url = "/imports/csvImport";
-    const apiUrl = "http://localhost:4040/api/v1" + url as string;
-    // const apiUrl = "https://lms-server-production-505b.up.railway.app/api/v1" + url as string;
+    // const apiUrl = "http://localhost:4040/api/v1" + url as string;
+    const apiUrl = "https://lms-server-production-505b.up.railway.app/api/v1" + url as string;
     const formData = new FormData();
     formData?.append("file", csvFile);
     formData?.append("subTopicId", data?.subTopicId);
